f0c04f21b39268a1c519b427d9d7811e04d8f0bb
YouKnow <hsn123.h7@gmail.com>
2023-12-28 11:40:14 +0300

    routes+server+db

---
+++ routes/reservation.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+router.use(express.json());
+
+const reservationCtrl = require('../controllers/reservation');
+
+//Routes
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ routes/stadium.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+router.use(express.json());
+
+const stadiumCtrl = require("../controllers/stadium");
+
+//Routes
+router.get("/add",stadiumCtrl.stadium_create_get);
+router.post("/add",stadiumCtrl.stadium_create_post);
+router.get("/index",stadiumCtrl.stadium_index_get);
+router.get("/detail",stadiumCtrl.stadium_show_get);
+router.delete("/delete",stadiumCtrl.stadium_delete_get);
+router.get("/edit",stadiumCtrl.stadium_edit_get);
+router.put("/update",stadiumCtrl.stadium_update_put);
+
+
+
+module.exports = router;
