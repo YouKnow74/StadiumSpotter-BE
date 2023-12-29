1864387d827c00abd6c29766eeaf81180557d18b
kwthr <k.mahfoodh2002@gmail.com>
2023-12-29 21:35:39 +0300

    uncomment user contoller and route

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 //RESTFUL API's for regestration and authentcation 
-const User =require('../models/User')
+const {User} =require('../models/User')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
