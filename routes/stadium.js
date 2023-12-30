1e86abb7d741fbb96baa435eda2f18171c790b95
YouKnow <hsn123.h7@gmail.com>
2023-12-30 18:06:39 +0300

    small changes stadium controller/route

--- routes/stadium.js
+++ routes/stadium.js
@@ -5,6 +5,22 @@ router.use(express.json());
 
 const stadiumCtrl = require("../controllers/stadium");
 
+//Multer 
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/')
+    },
+    filename: function (req, file, cb) {
+        let extArray = file.mimetype.split("/");
+        let extension = extArray[extArray.length - 1];
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + extension)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
 //Routes
 router.get("/add",stadiumCtrl.stadium_create_get);
 router.post("/add",stadiumCtrl.stadium_create_post);
